48d7f6477246efd17074b0fee07669f0cee37a6d
Brucknem <mbruckner94@gmail.com>
2019-02-04 01:49:03 +0100

    Click and keyboard working Save load working

---
+++ Python/PyCharm/src/HelloWorld.py
@@ -0,0 +1,16 @@
+# Code to check if left or right mouse buttons were pressed
+import win32api
+from time import sleep
+import pyautogui as autogui
+#import tkinter as tk
+import tkinter as tk
+from tkinter import filedialog
+import threading
+from datetime import datetime
+import keyboard  # using module keyboard
+import pythoncom, pyHook
+import os 
+import ctypes
+from ctypes import wintypes
+
+print("Fuuuuck")
