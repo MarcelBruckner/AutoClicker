809eb37fa1a59ade54cd45693b16a995330254f7
Brucknem <mbruckner94@gmail.com>
2019-02-18 23:51:05 +0100

    Human speed in spring adjusted

--- C#/AutoClicker/AutoClicker/Interpolations/MassSpringInterpolation.cs
+++ C#/AutoClicker/AutoClicker/Interpolations/MassSpringInterpolation.cs
@@ -11,7 +11,8 @@ namespace AutoClicker
 {
     public class MassSpringInterpolation : ICursorInterpolation
     {
-        private static readonly double DT = 0.05;
+        private static readonly double DELAY = 5000;
+        private static readonly double DT = 1.0 / DELAY;
         private static readonly double STIFFNESS = 0.5;
         private static readonly double DAMPING = 1.2;
 
@@ -30,12 +31,12 @@ namespace AutoClicker
             Vector direction = End - Cursor;
             Vector midpoint = Cursor + direction * random.NextDouble();
 
-            Points.Add(new MassPoint(End, new Vector(0, 0), -1, 0));
-            Points.Add(new MassPoint(midpoint, RandomNormal(End, Cursor, 50), random.NextDouble() * 2 + 1, DAMPING / 2));// random.NextDouble() * 0.8 + 0.6));
-            Points.Add(new MassPoint(Cursor, RandomVector(50), random.NextDouble() * 2 + 1, DAMPING));
+            Points.Add(new MassPoint(End, new Vector(0, 0), -1, 0, Speed));
+            //Points.Add(new MassPoint(midpoint, RandomNormal(End, Cursor, 50), random.NextDouble() * 2 + 1, DAMPING / 2));// random.NextDouble() * 0.8 + 0.6));
+            Points.Add(new MassPoint(Cursor, RandomVector(50), random.NextDouble() * 2 + 1, DAMPING, Speed));
 
             springs.Add(new Spring(this, 0, 1, STIFFNESS, TargetRadius));
-            springs.Add(new Spring(this, 1, 2, STIFFNESS, TargetRadius));
+            //springs.Add(new Spring(this, 1, 2, STIFFNESS, TargetRadius));
         }
 
         public override void Interpolate()
@@ -61,7 +62,7 @@ namespace AutoClicker
                 }
 
                 Cursor = Points[Points.Count - 1].Position;
-                Thread.Sleep(Math.Max(1, (int)(1.0 / (DT * Speed * 100))));
+                //Thread.Sleep((int)Math.Max(1, DELAY / 50));
             }
         }
 
@@ -72,7 +73,7 @@ namespace AutoClicker
             public double Stiffness { get; private set; }
             public double InitialLength { get; private set; }
             private MassSpringInterpolation outer;
-            private int targetRadius;
+            public int TargetRadius { get; private set; }
 
             public Spring(MassSpringInterpolation outer, int start, int end, double stiffness, int targetRadius)
             {
@@ -80,7 +81,7 @@ namespace AutoClicker
                 Start = start;
                 End = end;
                 Stiffness = stiffness;
-                this.targetRadius = targetRadius;
+                TargetRadius = targetRadius;
             }
 
             public void ComputeElasticForce()
@@ -92,14 +93,14 @@ namespace AutoClicker
                 outer.Points[Start].Force += -force;
                 outer.Points[End].Force += force;
 
-                int randomLength = 10 * targetRadius;
-                if (length < 20 * targetRadius)
+                int randomLength = 10 * TargetRadius;
+                if (length < 20 * TargetRadius)
                 {
-                    randomLength = 5 * targetRadius;
+                    randomLength = 5 * TargetRadius;
                 }
-                else if (length < 3 * targetRadius)
+                else if (length < 3 * TargetRadius)
                 {
-                    randomLength = targetRadius / 2;
+                    randomLength = TargetRadius / 2;
                 }
                 outer.Points[End].RandomVelocity += RandomNormal(outer.Points[Start].Position, outer.Points[End].Position, randomLength);
                 outer.Points[Start].RandomVelocity += RandomNormal(outer.Points[Start].Position, outer.Points[End].Position, randomLength);
@@ -114,27 +115,29 @@ namespace AutoClicker
             public Vector Force { get; set; }
             public double Mass { get; private set; }
             public double Damping { get; private set; }
+            public double Speed { get; private set; }
 
-            public MassPoint(Vector position, Vector velocity, double mass, double damping)
+            public MassPoint(Vector position, Vector velocity, double mass, double damping, double speed)
             {
                 Position = position;
                 Velocity = velocity;
                 Force = new Vector(0, 0);
                 Mass = mass;
                 Damping = damping;
+                Speed = speed;
             }
 
             public void IntegratePosition()
             {
                 if (Mass > 0)
                 {
-                    Position += DT * Velocity;
+                    Position += DT / Speed * Velocity;
                 }
             }
 
             public void IntegrateVelocity()
             {
-                Velocity += DT * ((Force - Damping * Velocity) / Mass + RandomVelocity);
+                Velocity += DT / Speed * ((Force - Damping * Velocity) / Mass + RandomVelocity);
             }
 
             public double Distance(MassPoint other)
